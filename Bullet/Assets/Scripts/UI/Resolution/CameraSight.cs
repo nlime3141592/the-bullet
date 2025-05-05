396b04e88d9f4b7de47a39915ca199c6ea77e33f
YellowGreenLime <nlime3141592@naver.com>
2025-05-06 04:02:06 +0900

    [Fix] 해상도에 따라 보이는 영역의 가로 길이가 일정하도록 카메라 FOV를 제어

--- Bullet/Assets/Scripts/UI/Resolution/CameraSight.cs
+++ Bullet/Assets/Scripts/UI/Resolution/CameraSight.cs
@@ -1,55 +1,114 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraSight : MonoBehaviour
+namespace Unchord
 {
-    public float displaysight = 5;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CameraSight : MonoBehaviour
     {
-        setting_sight();
-    }
+        #region Inspector Properties
+        public float targetDepth = 9.0f;
+        public float targetDistance = 5.848f;
 
-    // Update is called once per frame
-    public bool test = false;
-    void Update()
-    {
-        if (test == true)
-        { test = false; setting_sight(); }
-    }
+        public float yPosOnIdle = 3.0f;
+        public float yPosOnGame = 4.5f;
 
-    // 카메라 시야를 설정하는 함수
-    void setting_sight()
-    {
-        Camera cam = GameObject.FindObjectOfType<Camera>();
+        public float traceSpeed = 3.5f;
+        #endregion
 
-        Vector2 screenSize = new Vector2(cam.pixelWidth, cam.pixelHeight);
+        private Camera _camera;
+        private float _aspect;
+        private float _posY;
 
-        Vector3 pxancmin = new Vector3(0, screenSize.y / 2, 5);
-        Vector3 pxancmax = new Vector3(screenSize.x, screenSize.y / 2, 5);
+        public void SetPositionForIdle()
+        {
+            _posY = yPosOnIdle;
+        }
 
-        Vector3 ancmin2world = cam.ScreenToWorldPoint(pxancmin);
-        Vector3 ancmax2world = cam.ScreenToWorldPoint(pxancmax);
+        public void SetPositionForIdleImmediate()
+        {
+            _posY = yPosOnIdle;
+            transform.position = new Vector3(transform.position.x, _posY, transform.position.z);
+        }
 
-        float dx = Mathf.Abs(ancmax2world.x - ancmin2world.x);
+        public void SetPositionForGame()
+        {
+            _posY = yPosOnGame;
+        }
 
-        float camsize = cam.fieldOfView;
+        public void SetPositionForGameImmediate()
+        {
+            _posY = yPosOnGame;
+            transform.position = new Vector3(transform.position.x, _posY, transform.position.z);
+        }
 
-        float ratio = displaysight / dx;
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            
+            SetPositionForIdleImmediate();
+            SetFOV();
+        }
 
-        camsize *= ratio;
+        private void Update()
+        {
+            UpdateFOV();
+            UpdatePosY();
+        }
 
-        cam.fieldOfView = camsize;
-    }
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            if (_camera == null)
+                _camera = GetComponent<Camera>();
 
-    void logger(Vector3 vec)
-    {
-        Debug.Log(vec.x + " " + vec.y + " " + vec.z);
+            float x = _camera.pixelWidth;
+            float y = _camera.pixelHeight * 0.5f;
+            float z = targetDepth;
 
-        GameObject block = Resources.Load("Prefabs/Block/Block00") as GameObject;
-        GameObject obj = GameObject.Instantiate(block, vec, Quaternion.identity);
-        obj.GetComponent<MeshRenderer>().material = Resources.Load("Material/BulletHeadColor/Color00") as Material;
+            Vector3 anchorLeft0 = new Vector3(0.0f, y, _camera.nearClipPlane);
+            Vector3 anchorLeft1 = new Vector3(0.0f, y, targetDepth);
+            Vector3 anchorRight0 = new Vector3(x, y, _camera.nearClipPlane);
+            Vector3 anchorRight1 = new Vector3(x, y, targetDepth);
+
+            Vector3 pointLeft0 = _camera.ScreenToWorldPoint(anchorLeft0);
+            Vector3 pointLeft1 = _camera.ScreenToWorldPoint(anchorLeft1);
+            Vector3 pointRight0 = _camera.ScreenToWorldPoint(anchorRight0);
+            Vector3 pointRight1 = _camera.ScreenToWorldPoint(anchorRight1);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(pointLeft0, pointRight0);
+            Gizmos.DrawLine(pointLeft0, pointLeft1);
+            Gizmos.DrawLine(pointLeft1, pointRight1);
+            Gizmos.DrawLine(pointRight0, pointRight1);
+
+            Debug.Log(Vector3.Distance(pointLeft1, pointRight1));
+        }
+#endif
+
+        private void UpdateFOV()
+        {
+            if (_aspect != _camera.aspect)
+            {
+                _aspect = _camera.aspect;
+                SetFOV();
+            }
+        }
+
+        private void UpdatePosY()
+        {
+            if (Mathf.Abs(transform.position.y - _posY) < 0.001f)
+                return;
+
+            float y = Mathf.Lerp(transform.position.y, _posY, traceSpeed * Time.deltaTime);
+
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+
+        private void SetFOV()
+        {
+            float targetHeight = targetDistance / _camera.aspect;
+            float fov = 2.0f * Mathf.Atan(0.5f * targetHeight / targetDepth) * Mathf.Rad2Deg;
+
+            _camera.fieldOfView = fov;
+        }
     }
 }
